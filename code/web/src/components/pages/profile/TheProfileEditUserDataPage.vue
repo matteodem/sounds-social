f30e1c324f356ade5fc1d235f4c831a112c2194e
Matteo De Micheli <matteodem@protonmail.com>
2018-04-13 14:42:37 +0200

    Adjust title size for data page

--- code/web/src/components/pages/profile/TheProfileEditUserDataPage.vue
+++ code/web/src/components/pages/profile/TheProfileEditUserDataPage.vue
@@ -4,7 +4,7 @@
       <header-component current="profile"></header-component>
     </div>
     <div slot="main">
-      <pure-title size="f-subheadline" v-text="$t('Manage user data')"></pure-title>
+      <pure-title size="f-subheadline-l f1" v-text="$t('Manage user data')"></pure-title>
 
       <div class="cf mw-none-l mw6">
         <div class="fl w-50-l w-100">
@@ -20,7 +20,7 @@
           <h2 class="orange" v-text="$t('Delete user')"></h2>
 
           <p class="lh-copy">
-            <span v-text="$t('Want to leave Sounds Social?')"></span>
+            <span v-text="$t('Want to delete your user on Sounds Social?')"></span>
             <br />
             <span v-text="$t('You will be guided through a simple process to do just that.')"></span>
           </p>
@@ -31,12 +31,111 @@
 
       <pure-button class="mt4" @click="$routeNavigator.openProfileEdit($route.params.id, 'user')"
                    v-text="$t('Back to profile settings')"></pure-button>
+
+      <!-- TODO: reuse h2 titles: modal-title component -->
+      <!-- TODO: create modal-process component -->
+      <pure-modal @close="abortDeleteProcess" v-if="openModalId === 'explain'">
+        <div class="pa4">
+          <div class="lh-copy">
+            <h2 class="tc f2" v-text="$t('What will be permanently deleted?')"></h2>
+
+            <span class="b" v-text="$t('Following data will be deleted forever')"></span>:
+            <ul>
+              <li v-text="$t('Your profile data')"></li>
+              <li v-text="$t('Your playlists')"></li>
+              <li v-text="$t('Your aliases (where you are the only member)')"></li>
+              <li v-text="$t('Metadata of sounds that you uploaded (title, description, etc. including aliases)')"></li>
+            </ul>
+          </div>
+
+          <div class="dib mr2">
+            <pure-button @click="abortDeleteProcess" color="orange" v-text="$t('Abort')"></pure-button>
+          </div>
+
+          <pure-button @click="continueDeleteProcess" v-text="$t('Continue')"></pure-button>
+        </div>
+      </pure-modal>
+
+      <pure-modal @close="abortDeleteProcess" v-if="openModalId === 'passphraseExplain'">
+        <div class="pa4">
+          <div class="lh-copy">
+            <h2 class="tc f2" v-text="$t('What will continue being accessible?')"></h2>
+
+            <span class="b" v-text="$t('Following data will still be accessible with your passphrase')"></span>:
+            <ul>
+              <li v-text="$t('Your uploaded sounds (audio file)')"></li>
+              <li v-text="$t('Your uploaded covers, profile avatars (image files)')"></li>
+            </ul>
+
+            <span v-text="$t('The files will be inaccessible to others after deletion')"></span>.
+            <br />
+            <span v-text="$t('Your passphrase is the key to')"></span>
+            <!-- TODO: link -->
+            <a class="link blue" href="/#" target="_blank"  v-text="$t('access the files after deletion')"></a>.
+          </div>
+
+          <div class="mt3">
+            <div class="dib mr2">
+              <pure-button @click="abortDeleteProcess" color="orange" v-text="$t('Abort')"></pure-button>
+            </div>
+
+            <pure-button @click="continueDeleteProcess" v-text="$t('Continue')"></pure-button>
+          </div>
+        </div>
+      </pure-modal>
+
+      <pure-modal @close="abortDeleteProcess" v-if="openModalId === 'passphraseDisplay'">
+        <div class="pa4">
+          <div class="lh-copy">
+            <h2 class="tc f2" v-text="$t('Write down your passphrase')"></h2>
+
+            <!-- TODO: actually display passphrase -->
+            <div class="tc f2 white pa3 bg-light-gray" v-text="'water wow nice okay'"></div>
+
+            <div class="tc mt3">
+              <span v-text="$t('Your passphrase will not be recoverable after deletion')"></span>!
+            </div>
+          </div>
+
+          <div class="mt3">
+            <div class="dib mr2">
+              <pure-button @click="abortDeleteProcess" color="orange" v-text="$t('Abort')"></pure-button>
+            </div>
+
+            <pure-button @click="continueDeleteProcess" v-text="$t('Continue')"></pure-button>
+          </div>
+        </div>
+      </pure-modal>
+
+      <pure-modal @close="abortDeleteProcess" v-if="openModalId === 'confirm'">
+        <div class="pa4">
+          <div class="lh-copy">
+            <h2 class="tc f2" v-text="$t('Confirm deletion')"></h2>
+
+            <div class="tc mt3">
+              <span v-text="$t('After careful consideration of the previous information')"></span>,
+              <span v-text="$t('you can now confirm deleting your user')"></span>.
+            </div>
+          </div>
+
+          <div class="mt3">
+            <div class="dib mr2">
+              <pure-button @click="abortDeleteProcess" color="orange" v-text="$t('Abort')"></pure-button>
+            </div>
+
+            <pure-button @click="deleteUser" :fill="true" v-text="$t('Delete')"></pure-button>
+          </div>
+        </div>
+      </pure-modal>
+
     </div>
   </layout-default>
 </template>
 <script>
   import HeaderComponent from '../../stateful/StatefulHeader.vue'
   import { downloadUserExportData } from '../../../api/UserApi'
+
+  const modalIds = ['explain', 'passphraseExplain', 'passphraseDisplay', 'confirm']
 
   export default {
     components: {
@@ -47,12 +146,26 @@
         title: this.$t('User data'),
       }
     },
+    data () {
+      return {
+        openModalId: null,
+      }
+    },
     methods: {
       downloadExport () {
         downloadUserExportData()
       },
       startDeleteProcess () {
-
+        this.openModalId = modalIds[0]
+      },
+      abortDeleteProcess () {
+        this.openModalId = null
+      },
+      continueDeleteProcess () {
+        this.openModalId = modalIds[modalIds.indexOf(this.openModalId) + 1]
+      },
+      deleteUser () {
+        // TODO: delete user through API, log out, display simple window.alert
       },
     },
   }
